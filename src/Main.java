a2caaea7b8fdc9f389983c360f102cfa4ac07152
marcelo escamilla <mescamillas@unal.edu.co>
2020-05-23 21:03:06 -0500

    chocopy grammar, some errors need fixing

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try{
+            // crear un analizador léxico que se alimenta a partir de la entrada (archivo  o consola)
+            ChocoPyLexer lexer;
+            if (args.length>0)
+                lexer = new ChocoPyLexer(CharStreams.fromFileName(args[0]));
+            else
+                lexer = new ChocoPyLexer(CharStreams.fromStream(System.in));
+            // Identificar al analizador léxico como fuente de tokens para el sintactico
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            // Crear el objeto correspondiente al analizador sintáctico que se alimenta a partir del buffer de tokens
+            ChocoPyParser parser = new ChocoPyParser(tokens);
+            ParseTree tree = parser.program(); // Iniciar el analisis sintáctico en la regla inicial: r
+            System.out.println(tree.toStringTree(parser)); // imprime el arbol al estilo LISP
+        } catch (Exception e){
+            System.err.println("Error (Test): " + e);
+        }
+    }
+}
+
